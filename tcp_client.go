17999825d6fec7913d3420ee0eba5f9c359f7873
Omarp321 <Omarpassi73@gmail.com>
2023-04-26 17:10:37 +0000

    nyeste

--- tcp_client.go
+++ tcp_client.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"net"
 	"log"
+	"net"
 	"os"
+
+	"github.com/Omarp321/is105sem03/mycrypt"
 )
 
 func main() {
@@ -11,18 +13,22 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-    
-	log.Println("os.Args[1] = ", os.Args[1])
 
- 	_, err = conn.Write([]byte(os.Args[1]))
+	// Krypterer meldingen
+	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+	log.Println("Kryptert melding: ", string(kryptertMelding))
+
+	_, err = conn.Write([]byte(string(kryptertMelding)))
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	buf := make([]byte, 1024)
 	n, err := conn.Read(buf)
 	if err != nil {
 		log.Fatal(err)
-	} 
+	}
 	response := string(buf[:n])
 	log.Printf("reply from proxy: %s", response)
 }
+
